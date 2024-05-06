ccf7ec70470975cc5ffa366c2dd421e36880c764
liaoguotong <gtoon@qq.com>
2024-05-06 12:04:13 +0800

    修改链接

--- themes/heo/config.js
+++ themes/heo/config.js
@@ -5,8 +5,8 @@ const CONFIG = {
 
   // 首页顶部通知条滚动内容，如不需要可以留空 []
   HEO_NOTICE_BAR: [
-    { title: '欢迎来到ifree', url: 'https://blog.tangly1024.com' },
-    { title: '正义都能迟到 为什么上班不行', url: 'https://docs.tangly1024.com' }
+    { title: '欢迎来到ifree', url: 'https://mp.weixin.qq.com/s/C9XvZYeOoiA-IaJrFb3y4g' },
+    { title: '正义都能迟到 为什么上班不行', url: 'https://mp.weixin.qq.com/s/C9XvZYeOoiA-IaJrFb3y4g' }
   ],
 
   // 英雄区左右侧组件颠倒位置
@@ -108,7 +108,7 @@ const CONFIG = {
   HEO_SOCIAL_CARD_TITLE_1: '交流频道',
   HEO_SOCIAL_CARD_TITLE_2: '加入我们的社群讨论分享',
   HEO_SOCIAL_CARD_TITLE_3: '点击加入社群',
-  HEO_SOCIAL_CARD_URL: 'https://docs.tangly1024.com/article/how-to-question',
+  HEO_SOCIAL_CARD_URL: 'https://mp.weixin.qq.com/s/C9XvZYeOoiA-IaJrFb3y4g',
 
   // *****  以下配置无效，只是预留开发 ****
   // 菜单配置
